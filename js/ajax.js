d59405ee81fcc7699bff7e897275a91795517b09
gghidoni <47367474+gghidoni@users.noreply.github.com>
2022-05-26 14:05:14 +0200

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -29,37 +29,10 @@
         let posiz = $('input[name="posiz[]"]:checked').map(function(){
           return $(this).val();
         }).get();
-        
         let caratt = $('input[name="caratt[]"]:checked').map(function(){
           return $(this).val();
         }).get();
-        // let prezzo_min = $('input[name="prezzo_min[]"]:checked').map(function(){
-        //   return $(this).val();
-        // }).get();
-        // let prezzo_max = $('input[name="prezzo_max[]"]:checked').map(function(){
-        //   return $(this).val();
-        // }).get();
-        // let dim_min = $('input[name="dim_min[]"]:checked').map(function(){
-        //   return $(this).val();
-        // }).get();
-        // let dim_max = $('input[name="dim_max[]"]:checked').map(function(){
-        //   return $(this).val();
-        // }).get();
 
-
-        console.log('stat = ' + stat);
-        console.log('posiz = ' + posiz);
-        console.log('tipo = ' + tipo);
-        console.log('caratt = ' + caratt);
-        console.log('prezzo_min = ' + prezzo_min);
-        console.log('prezzo_max = ' + prezzo_max);
-        console.log('camere_min = ' + camere_min);
-        console.log('bagni_min = ' + bagni_min);
-        console.log('garages_min = ' + garages_min);
-        console.log('dim_min = ' + dim_min);
-        console.log('dim_max = ' + dim_max);
-
-        
         
           $.ajax({
               type: "POST",
@@ -94,21 +67,15 @@
           })
 
 
-
-
-
     
       $('#btn-search').on('click', function(){
         $('.search-panel__result-box').html("");
         console.clear();
 
-        // let prezzo_min = $('select[name="prezzo_min"]').val();
-        // let prezzo_max = $('select[name="prezzo_max"]').val();
+
         let camere_min = $('input[name="camere_min"]').val();
         let bagni_min = $('input[name="bagni_min"]').val();
         let garages_min = $('input[name="garages_min"]').val();
-        // let dim_min = $('select[name="dim_min"]').val();
-        // let dim_max = $('select[name="dim_max"]').val();
 
         let stat = $('input[name="stat[]"]:checked').map(function(){
           return $(this).val();
@@ -150,18 +117,7 @@
           dim_max = ["99999", ];
         }
 
-        console.log('stat = ' + stat);
-        console.log('posiz = ' + posiz);
-        console.log('tipo = ' + tipo);
-        console.log('caratt = ' + caratt);
-        console.log('prezzo_min = ' + prezzo_min);
-        console.log('prezzo_max = ' + prezzo_max);
-        console.log('camere_min = ' + camere_min);
-        console.log('bagni_min = ' + bagni_min);
-        console.log('garages_min = ' + garages_min);
-        console.log('dim_min = ' + dim_min);
-        console.log('dim_max = ' + dim_max);
-        
+
           $.ajax({
               type: "POST",
                url: novastart_obj.admin_url,
